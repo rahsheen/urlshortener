43610f43beb9b801146924a6b24abff7c1d0a557
R. Anthony Porter <rah@sheenonline.biz>
2017-04-01 04:46:07 -0400

    Completed basic functionality.

--- app.js
+++ app.js
@@ -4,22 +4,84 @@ var shortid = require('shortid');
 var mongodb = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 5000;
+var url = "mongodb://localhost:27017/urlshortener";
 
 var app = express();
 
-var myMware = function (req, res) {
+var shortRoute = function (req, res) {
+	var theID = req.params.id;
+
+	mongodb.connect(url, function (err, db) {
+		if (err) throw err;
+
+		var urlCollection = db.collection('urls');
+
+		// See if we have a URL for this ID
+		urlCollection.findOne({
+			short: theID
+		}).then(function (existsURL) {
+			if (existsURL) {
+				return res.redirect(301, existsURL.original);
+			} else {
+				res.status(404).end();
+			}
+
+			db.close();
+		});
+	});
+};
+
+var newRoute = function (req, res) {
 	var oURL = req.originalUrl;
-	var theURL = oURL.slice(5,oURL.length);
+	var theURL = oURL.slice(5, oURL.length);
 
 	if (validURL.isWebUri(theURL)) {
-		res.status(200).send(JSON.stringify(theURL));
+
+		mongodb.connect(url, function (err, db) {
+			if (err) throw err;
+
+			// See if the URL already exists
+			var urlCollection = db.collection('urls');
+
+			urlCollection.findOne({
+				original: theURL
+			}).then(function (existsURL) {
+
+				if (existsURL) {
+					// URL already in DB
+
+					res.status(200).send(JSON.stringify(existsURL));
+
+					db.close();
+				} else {
+					// Store URL in DB
+					var theID = shortid.generate();
+
+					var urlInfo = {
+						original: theURL,
+						short: theID
+					};
+
+					urlCollection.insert(urlInfo, function (err, data) {
+						if (err) throw err;
+
+						res.status(200).send(JSON.stringify(urlInfo));
+						db.close();
+					});
+				}
+			});
+		});
 	} else {
-		console.log(req.params.url);
-		res.status(404).send("Invalid URL! --> " +theURLn);
+		var error = {
+			"error": "Wrong URL format."
+		};
+
+		res.status(404).send(JSON.stringify(error));
 	}
 }
 
-app.get('/new/*', myMware);
+app.get('/new/*', newRoute);
+app.get('/:id', shortRoute);
 
 app.use(express.static('public'));
 
